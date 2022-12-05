c4e0786b9f000601063685d4b8e9742635b4c3d3
Brian Leip <brianleip@gmail.com>
2022-12-04 23:23:01 -0800

    progress on 3.3

--- 03-stacks-queues.js
+++ 03-stacks-queues.js
@@ -4,8 +4,19 @@
 // https://leetcode.com/discuss/general-discussion/1152824/cracking-the-coding-interview-6th-edition-in-leetcode
 
 // CLASSES FOR CH 3 EXAMPLES
+// NODE (used in both Stack and Queue)
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 // STACK
+class Stack {
+  constructor(node)
 
+
+}
 
 // QUEUE
 
@@ -20,6 +31,57 @@
 
 
 // 3.3 Stack of Plates: Imagine a (literal) stack of plates. If the stack gets too high, it might topple. Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. Implement a data structure SetOfStacks that mimics this. SetO-fStacks should be composed of several stacks and should create a new stack once the previous one exceeds capacity. SetOfStacks. push() and SetOfStacks. pop() should behave identically to a single stack (that is, pop() should return the same values as it would if there were just a single stack).  FOLLOW UP: Implement a function popAt(int index) which performs a pop operation on a specific sub-stack.  Hints:#64, #87
+
+class DinnerPlates {
+  constructor(capacity) {
+    this.capacity = capacity > 0 ? capacity : 1;
+    this.stacks = [[]];
+    this.leftmostOpen = 0;
+    this.rightmost = 0;
+  }
+};
+
+DinnerPlates.prototype.push = function(val) {
+    // starting at leftmostOpen, check if at capacity and find the leftmost stack that's not at capacity
+    console.log(this.stacks[this.leftmostOpen].length + " vs " + this.capacity)
+    while (this.stacks[this.leftmostOpen].length >= this.capacity) {
+      this.leftmostOpen += 1;
+    }
+    // make sure rightmost >= leftmostOpen
+    if (this.rightmost < this.leftmostOpen)
+      this.rightmost = this.leftmostOpen;
+    console.log("pushing " + val + " to stack " + this.leftmostOpen);
+    // create a new stack if needed
+    if (this.stacks[this.leftmostOpen] === undefined)
+      this.stacks[this.leftmostOpen].push(val);
+    // else push to the top of leftmostOpen stack
+    else
+      this.stacks[this.leftmostOpen].push(val);
+};
+
+DinnerPlates.prototype.pop = function() {
+  // return -1 if all stacks are empty
+  if (this.stacks.length === 0)
+    return -1;
+  // else pop from rightmost stack
+  let val = this.stacks[this.rightmost].pop();
+  if (this.stacks[this.rightmost].length === 0) {
+    this.rightmost -= 1;
+  // make sure leftmostOpen <= rightmost
+  if (this.leftmostOpen > this.rightmost)
+    this.leftmostOpen = this.rightmost;
+  }
+  return val;
+};
+
+DinnerPlates.prototype.popAtStack = function(index) {
+  if (this.stacks[index].length === 0)
+    return -1;
+  let val = this.stacks[index].pop();
+  if (index < this.leftmostOpen)
+    this.leftmostOpen = index;
+  return val;
+};
 
 
 // 3.4 Queue via Stacks: Implement a MyQueue class which implements a queue using two stacks.  Hints: #98, #7 74
